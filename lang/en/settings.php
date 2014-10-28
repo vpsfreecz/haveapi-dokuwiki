e06376eeffcd176a90bc6e70cd1a83f806d8bd4a
Jakub Skokan <jakub.skokan@havefun.cz>
2014-10-28 13:04:31 +0100

    Localize settings

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -6,8 +6,13 @@
  */
 
 // keys need to match the config setting name
-// $lang['fixme'] = 'FIXME';
-
+$lang['api_url']             = 'API URL';
+$lang['api_version']         = 'API version, defaults to default version';
+$lang['client_identity']     = 'Client identity (sent as user agent to the API)';
+$lang['user_resource']       = "Path to resource that returns user information, nested resources separated by '.' (dot)";
+$lang['user_current_action'] = 'Action of user_resource that returns information about currently logged user';
+$lang['user_name']           = "Parameter of user_resource containing user's full name";
+$lang['user_mail']           = "Parameter of user_resource containing user's email";
 
 
 //Setup VIM: ex: et ts=4 :
